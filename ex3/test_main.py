f0e6e2d4680614b375de36bdeae4dd957eccbcaa
Charles Huet <charles.huet@ynov.com>
2021-04-22 17:03:28 +0200

    Update test_main.py

--- ex3/test_main.py
+++ ex3/test_main.py
@@ -9,7 +9,6 @@ class TestExercice3(unittest.TestCase):
     def test_split_str_by_len(self):
         test_ex = Exercice3()
         self.assertEqual('My string is so long, i have to add carriage returns', test_ex.split_string_by_len('My string is so long, i have to add carriage returns', 5))
-        self.assertEqual('Hello-World', test_ex.join_string_list(['Hello', 'World'], "-"))
 
 
 
